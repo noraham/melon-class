6c878c23dc80f5ebb5e0a267079b20fc64b9b246
Nora Hammack <noraham@gmail.com>
2018-01-16 22:30:24 -0800

    assignment complete

--- melons.py
+++ melons.py
@@ -33,4 +33,9 @@ class Melon(object):
 
 # FIXME: Add Squash class definition here.
 class Squash(Melon):
-    pass
+    """omg a squash"""
+
+    def prep(self):
+        robots.cleanerbot.clean(self)
+        robots.stickerbot.apply_logo(self)
+        robots.painterbot.paint(self)
